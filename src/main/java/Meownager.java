9588c41c64e0b770e327e08c9ef946032b0193c6
Yu Tingan <163671620+llamawithhats@users.noreply.github.com>
2025-08-20 02:34:24 +0800

    Add Level 0 (Rename, Greet, Exit)

---
+++ src/main/java/Meownager.java
@@ -0,0 +1,22 @@
+public class Meownager {
+
+    public static void line() {
+        System.out.println("_".repeat(30));
+    }
+
+    public static void main(String[] args) {
+
+        String logo = "  ( =＾･ω･＾=)ノ <3\n";
+
+        line();
+
+        System.out.println("Hello! I'm your resident MEOWnager!\n" + logo
+                + "Anything I can do for you neow?");
+
+        line();
+
+        System.out.println("Meow you next time!");
+
+    }
+}
+
