378e78af2faf7b6584b47730b0069c4571bf31ae
Yu Tingan <163671620+llamawithhats@users.noreply.github.com>
2025-08-22 19:08:03 +0800

    Added Echo

--- src/main/java/Meownager.java
+++ src/main/java/Meownager.java
@@ -1,21 +1,32 @@
+import java.util.Scanner;
+
+//NOTE: IdeaProjects -> ip -> src -> main -> java
 public class Meownager {
 
-    public static void line() {
-        System.out.println("_".repeat(30));
+    public static void greetings() {
+        String logo = "  ( =＾･ω･＾=)ノ <3\n";
+
+        System.out.println("Hello! I'm your resident MEOWnager!\n" + logo
+                            + "Anything I can do for you neow?\n");
+    }
+
+
+    public static void echo(Scanner sc) {
+        String input = sc.nextLine();
+
+        if (input.equals("bye")) {
+            System.out.println("\n\tMeow you next time!");
+        } else {
+            System.out.println("\n\t" + input);
+            echo(sc);
+        }
     }
 
     public static void main(String[] args) {
 
-        String logo = "  ( =＾･ω･＾=)ノ <3\n";
-
-        line();
-
-        System.out.println("Hello! I'm your resident MEOWnager!\n" + logo
-                + "Anything I can do for you neow?");
-
-        line();
-
-        System.out.println("Meow you next time!");
+        greetings();
+        Scanner sc = new Scanner(System.in);
+        echo(sc);
 
     }
 }
