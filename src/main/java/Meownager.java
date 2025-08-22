61e8125ce1d78cf46b19c28d70827b16c7a29233
Yu Tingan <163671620+llamawithhats@users.noreply.github.com>
2025-08-22 22:11:43 +0800

    Added Add, List

--- src/main/java/Meownager.java
+++ src/main/java/Meownager.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 //NOTE: IdeaProjects -> ip -> src -> main -> java
+//sout: System.out.println
+
 public class Meownager {
 
     public static void greetings() {
@@ -10,24 +12,33 @@ public class Meownager {
                             + "Anything I can do for you neow?\n");
     }
 
-
-    public static void echo(Scanner sc) {
+    public static void addList(Scanner sc, int index, String[] listOfTasks) {
         String input = sc.nextLine();
 
         if (input.equals("bye")) {
             System.out.println("\n\tMeow you next time!");
+        } else if (input.equals("list")) {
+            if (index == 0) {
+                System.out.println("\n\t" + "1. " + null);
+            }
+            for (int i = 0; i < index; i++) {
+                System.out.println("\n\t" + (i+1) + ". " + listOfTasks[i]);
+            }
+            addList(sc, index, listOfTasks); //repeat inputs
         } else {
-            System.out.println("\n\t" + input);
-            echo(sc);
+            System.out.println("\n\tadded: " + input);
+            listOfTasks[index] = input;
+            index++; //increment array position
+            addList(sc, index, listOfTasks);
         }
     }
 
     public static void main(String[] args) {
-
         greetings();
         Scanner sc = new Scanner(System.in);
-        echo(sc);
-
+        String[] listOfTasks = new String[100];
+        addList(sc, 0, listOfTasks);
     }
+
 }
 
