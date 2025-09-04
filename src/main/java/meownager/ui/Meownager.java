6ee6c39cda87b6bb7d3821bc1142b811a7b3f74b
Yu Tingan <163671620+llamawithhats@users.noreply.github.com>
2025-09-04 19:39:43 +0800

    Transfer some methods from Meownager to Parser

--- src/main/java/meownager/ui/Meownager.java
+++ src/main/java/meownager/ui/Meownager.java
@@ -17,6 +17,8 @@ public class Meownager {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
+    private Parser parser;
+    private static final String FILE_PATH = "./data/Meownager.txt";
 
     /**
      * Constructs a new Meownager object with the given file path
@@ -30,6 +32,7 @@ public class Meownager {
     public Meownager(String filePath) {
         storage = new Storage(filePath);
         ui = new Ui();
+        parser = new Parser();
         
         try {
             storage.ensureFileExists();
@@ -53,7 +56,7 @@ public class Meownager {
                 exit();
                 break;
             }
-            handleCommand(input);
+            parser.handleCommand(input, tasks);
         }
         sc.close();
     }
@@ -70,117 +73,8 @@ public class Meownager {
         ui.showFarewell();
     }
 
-    /**
-     * Handles commands by the user ('list', 'mark', 'unmark', 'delete' and addList commands).
-     *
-     * @param input Input from the user.
-     */
-    private void handleCommand(String input) {
-        try {
-            if (input.equals("list")) {
-                handleList();
-            } else if (input.startsWith("mark ") || input.startsWith("unmark ") || input.startsWith("delete")) {
-                handleModifyList(input);
-            } else if (input.startsWith("find")) {
-                handleFind(input);
-            } else {
-                handleAddList(input);
-            }
-        } catch (MeownagerException e) {
-            ui.showError(e.getMessage());
-        }
-    }
-
-    /**
-     * Handles 'list' command.
-     *
-     * @throws MeownagerException If list is empty (no tasks).
-     */
-    private void handleList() throws MeownagerException {
-        if (tasks.isEmpty()) { //no tasks
-            throw MeownagerException.emptyList();
-        } else {
-            ui.showTaskList(tasks.getListOfTasks());
-        }
-    }
-
-    /**
-     * Handles 'mark', 'unmark' and 'delete' commands.
-     *
-     * @param input Input from user.
-     * @throws MeownagerException If inputted task number is invalid.
-     */
-    private void handleModifyList(String input) throws MeownagerException {
-        // finding task no. of input
-        int num = Integer.parseInt(input.split(" ")[1]);
-        if (num <= 0 || num > tasks.size()) { //not a task number
-            throw MeownagerException.outOfBoundsTaskNumber(num);
-        }
-        Task t = tasks.get(num - 1);
-        if (input.startsWith("delete")) {
-            tasks.remove(t);
-            t.deleteMessage(t, tasks.size());
-        } else { //mark, unmark
-            t.markMessage(t, input);
-        }
-    }
-
-    /**
-     * Handles commands that adds to task list.
-     *
-     * @param input Input from user.
-     * @throws MeownagerException If invalid input.
-     */
-    private void handleAddList(String input) throws MeownagerException {
-        Task t = null;
-        TaskType type = Parser.detectType(input);
-        if (type == null) { //invalid input
-            throw MeownagerException.unknownCommand();
-        }
-        switch (type) {
-        case TODO:
-            if (input.strip().equals("todo")) {
-                throw MeownagerException.emptyDescription("todo");
-            }
-            String descriptionTodo = input.split("todo ")[1];
-            t = new Todo(descriptionTodo);
-            break;
-        case DEADLINE:
-            if (input.strip().equals("deadline")) {
-                throw MeownagerException.emptyDescription("deadline");
-            }
-            if (!input.contains("/by")) {
-                throw MeownagerException.missingDeadlineInfo();
-            }
-            String descriptionDead = input.split("deadline |/by")[1].trim();
-            String date = input.split("/by")[1].trim(); //get deadline
-            t = new Deadline(descriptionDead, date);
-            break;
-        case EVENT:
-            if (input.strip().equals("event")) {
-                throw MeownagerException.emptyDescription("event");
-            }
-            if (!input.contains("/from") || !input.contains("/to")) {
-                throw MeownagerException.missingEventInfo();
-            }
-            String descriptionEve = input.split("event |/from")[1].trim();
-            String from = input.split("/from | /to")[1]; //get from date
-            String to = input.split("/to ")[1]; //get to date
-            t = new Event(descriptionEve, from, to);
-            break;
-        }
-        tasks.add(t); //add task to list
-        ui.showTaskAdded(t, tasks.size());
-    }
-
-    private void handleFind(String input) {
-        String filter = input.split("find ")[1].trim();
-        ArrayList<Task> filteredTasks = tasks.getFoundTasks(filter);
-        ui.showFilteredTasks(filteredTasks);
-    }
-
     public static void main(String[] args) {
-        new Meownager("./data/Meownager.txt").run();
+        new Meownager(FILE_PATH).run();
     }
 }
 
--- src/main/java/meownager/ui/Parser.java
+++ src/main/java/meownager/ui/Parser.java
@@ -1,11 +1,16 @@
 package meownager.ui;
 
+import java.util.ArrayList;
+
 /**
  * Deals with making sense of the user command
  *
  * @author Yu Tingan
  */
 public class Parser {
+
+    private Ui ui = new Ui();
+
     static TaskType detectType(String input) {
         if (input.startsWith("todo")) {
             return TaskType.TODO;
@@ -16,5 +21,114 @@ public class Parser {
         } else {
             return null;
         }
+    }
+
+    /**
+     * Handles commands by the user ('list', 'mark', 'unmark', 'delete' and addList commands).
+     *
+     * @param input Input from the user.
+     */
+    public void handleCommand(String input, TaskList tasks) {
+        try {
+            if (input.equals("list")) {
+                handleList(tasks);
+            } else if (input.startsWith("mark ") || input.startsWith("unmark ") || input.startsWith("delete")) {
+                handleModifyList(input, tasks);
+            } else if (input.startsWith("find")) {
+                handleFind(input, tasks);
+            } else {
+                handleAddList(input, tasks);
+            }
+        } catch (MeownagerException e) {
+            ui.showError(e.getMessage());
+        }
+    }
+
+    /**
+     * Handles 'list' command.
+     *
+     * @throws MeownagerException If list is empty (no tasks).
+     */
+    private void handleList(TaskList tasks) throws MeownagerException {
+        if (tasks.isEmpty()) { //no tasks
+            throw MeownagerException.emptyList();
+        } else {
+            ui.showTaskList(tasks.getListOfTasks());
+        }
+    }
+
+    /**
+     * Handles 'mark', 'unmark' and 'delete' commands.
+     *
+     * @param input Input from user.
+     * @throws MeownagerException If inputted task number is invalid.
+     */
+    private void handleModifyList(String input, TaskList tasks) throws MeownagerException {
+        // finding task no. of input
+        int num = Integer.parseInt(input.split(" ")[1]);
+        if (num <= 0 || num > tasks.size()) { //not a task number
+            throw MeownagerException.outOfBoundsTaskNumber(num);
+        }
+        Task t = tasks.get(num - 1);
+        if (input.startsWith("delete")) {
+            tasks.remove(t);
+            t.deleteMessage(t, tasks.size());
+        } else { //mark, unmark
+            t.markMessage(t, input);
+        }
+    }
+
+    /**
+     * Handles commands that adds to task list.
+     *
+     * @param input Input from user.
+     * @throws MeownagerException If invalid input.
+     */
+    private void handleAddList(String input, TaskList tasks) throws MeownagerException {
+        Task t = null;
+        TaskType type = Parser.detectType(input);
+        if (type == null) { //invalid input
+            throw MeownagerException.unknownCommand();
+        }
+        switch (type) {
+        case TODO:
+            if (input.strip().equals("todo")) {
+                throw MeownagerException.emptyDescription("todo");
+            }
+            String descriptionTodo = input.split("todo ")[1];
+            t = new Todo(descriptionTodo);
+            break;
+        case DEADLINE:
+            if (input.strip().equals("deadline")) {
+                throw MeownagerException.emptyDescription("deadline");
+            }
+            if (!input.contains("/by")) {
+                throw MeownagerException.missingDeadlineInfo();
+            }
+            String descriptionDead = input.split("deadline |/by")[1].trim();
+            String date = input.split("/by")[1].trim(); //get deadline
+            t = new Deadline(descriptionDead, date);
+            break;
+        case EVENT:
+            if (input.strip().equals("event")) {
+                throw MeownagerException.emptyDescription("event");
+            }
+            if (!input.contains("/from") || !input.contains("/to")) {
+                throw MeownagerException.missingEventInfo();
+            }
+            String descriptionEve = input.split("event |/from")[1].trim();
+            String from = input.split("/from | /to")[1]; //get from date
+            String to = input.split("/to ")[1]; //get to date
+            t = new Event(descriptionEve, from, to);
+            break;
+        }
+        tasks.add(t); //add task to list
+        ui.showTaskAdded(t, tasks.size());
+    }
+
+    private void handleFind(String input, TaskList tasks) {
+        String filter = input.split("find ")[1].trim();
+        ArrayList<Task> filteredTasks = tasks.getFoundTasks(filter);
+        ui.showFilteredTasks(filteredTasks);
     }
 }
